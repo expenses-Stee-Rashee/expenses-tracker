45d0a8f2f338a1127dc4113240a2a6edffb22bf1
bagnalli <stevenbagnall@outlook.com>
2023-03-22 11:05:57 +0000

    rough form set up, route created for post request

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
@@ -6,6 +7,30 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/transactions', methods=['GET', 'POST'])
+def transactions():
+    if request.method == 'POST':
+        date = request.form['date']
+        category = request.form['category']
+        amount = request.form['amount']
+        note = request.form['note']
+
+        response = requests.post('https://api.example.com/transactions', data ={
+            'date': date,
+            'category': category,
+            'amount': amount,
+            'note': note
+        })
+        
+        if response.ok:
+            return 'Transaction Added'
+        else: 
+            return 'Failed to add transaction'
+
+    else:    
+        return render_template("transactions.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
